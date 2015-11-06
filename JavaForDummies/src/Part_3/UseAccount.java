eb5e57d001bb508dc974e66db5a55022cd0cd571
Ali-Gregory-QA <alaethea.gregory-gamblin@qa.com>
2015-11-06 17:33:46 +0000

    Continued chapter 3

--- JavaForDummies/src/Part_3/UseAccount.java
+++ JavaForDummies/src/Part_3/UseAccount.java
@@ -13,9 +13,10 @@ public class UseAccount {
 		
 		yourAccount.name = "Jave Q. Public";
 		yourAccount.address = "111 Consumer Street";
-		yourAccount.balance = 55.63;
+		yourAccount.balance = 55.63;		
 		
-		myAccount.display();
+		//Listing 7-6
+		/*myAccount.display();
 		
 		out.print(" plus £");
 		out.print(myAccount.getInterest(5.00));
@@ -28,6 +29,19 @@ public class UseAccount {
 		double yourInterestAmount = 
 				yourAccount.getInterest(yourInterestRate);
 		out.print(yourInterestAmount);
-		out.println(" interest ");
+		out.println(" interest ");*/
+		
+		//Listing 7-7
+		myAccount.balance = 24.02;
+		yourAccount.balance = 55.63;
+		
+		double myInterest = myAccount.getInterest(5.00);
+		double yourInterest = yourAccount.getInterest(7.00);
+		
+		out.printf("£%4.2f\n",  myInterest);
+		out.printf("£%5.2f\n",  myInterest);
+		out.printf("£%.2f\n",  myInterest);
+		out.printf("£%3.2f\n",  myInterest);
+		out.printf("£%.2f £%.2f",  myInterest, yourInterest);
 	}
 }
