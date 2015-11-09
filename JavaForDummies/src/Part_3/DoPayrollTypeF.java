5b7dd644a0a491065b5a158d9af2c256e6372d19
Ali-Gregory-QA <alaethea.gregory-gamblin@qa.com>
2015-11-09 17:30:23 +0000

    Updated for Listing 8-8

--- JavaForDummies/src/Part_3/DoPayrollTypeF.java
+++ JavaForDummies/src/Part_3/DoPayrollTypeF.java
@@ -1,10 +1,10 @@
 package Part_3;
 
-//Listing 8-6
 public class DoPayrollTypeF {
 	
 	public static void main(String args[]) {
-	
+		
+		//Listing 8-8
 		FullTimeEmployee ftEmployee = new FullTimeEmployee();
 		
 		ftEmployee.setName("Barry Burd");
@@ -12,13 +12,36 @@ public class DoPayrollTypeF {
 		ftEmployee.setWeeklySalary(5000.00);
 		ftEmployee.setBenefitDeduction(500.00);
 		ftEmployee.cutCheck(ftEmployee.findPaymentAmount());
-		System.out.println();
 		
 		PartTimeEmployee ptEmployee = new PartTimeEmployee();
 		
-		ptEmployee.setName("Steve Surace");
-		ptEmployee.setJobTitle("Driver");
+		ptEmployee.setName("Chris Apelian");
+		ptEmployee.setJobTitle("Computer Book Author");
 		ptEmployee.setHourlyRate(7.53);
-		ptEmployee.cutCheck(ptEmployee.findPaymentAmount(10));
+		ptEmployee.cutCheck(ptEmployee.findPaymentAmount(50));
+		
+		PartTimeEmployee ptoEmployee = new PartTimeEmployee();
+		
+		ptoEmployee.setName("Steve Surace");
+		ptoEmployee.setJobTitle("Driver");
+		ptoEmployee.setHourlyRate(7.53);
+		ptoEmployee.cutCheck(ptoEmployee.findPaymentAmount(50));
+		
+		//Listing 8-6
+//		FullTimeEmployee ftEmployee = new FullTimeEmployee();
+//		
+//		ftEmployee.setName("Barry Burd");
+//		ftEmployee.setJobTitle("CEO");
+//		ftEmployee.setWeeklySalary(5000.00);
+//		ftEmployee.setBenefitDeduction(500.00);
+//		ftEmployee.cutCheck(ftEmployee.findPaymentAmount());
+//		System.out.println();
+//		
+//		PartTimeEmployee ptEmployee = new PartTimeEmployee();
+//		
+//		ptEmployee.setName("Steve Surace");
+//		ptEmployee.setJobTitle("Driver");
+//		ptEmployee.setHourlyRate(7.53);
+//		ptEmployee.cutCheck(ptEmployee.findPaymentAmount(10));
 	}
 }
